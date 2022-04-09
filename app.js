e594215e72b801a91f6410236351ef2227a8a963
jeal0uspengu1n <sutej.sharma@yahoo.com>
2022-04-09 12:12:29 +0530

    tryinn

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.post("/login", function(req, res){
     }
   });
 });
-app.listen(6969, function() {
+app.listen(process.env.PORT || 6969, function() {
   console.log("Server started on port 3000.");
 });
